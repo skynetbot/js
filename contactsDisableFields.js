66f090d1b81908ebbe67ce7f97f7a24dd7a61b8d
Milton Reyes <miltonhreyes@gmail.com>
2015-09-11 09:38:25 -0400

    added contactTypeOnChange function to reevaluate previous coder function ontypecontactchange

--- contactsDisableFields.js
+++ contactsDisableFields.js
@@ -26,3 +26,33 @@ function OnTypeContactChange() {
         Xrm.Page.ui.tabs.get("general").sections.get("general_section_7").setVisible(false);
     }
 }
+function contactTypeOnChange() {
+    var contactTypeValue = Xrm.Page.getAttribute("new_contacttype").getValue();
+    var section5 = Xrm.Page.ui.tabs.get("general").sections.get("general_section_5");
+    var section6 = Xrm.Page.ui.tabs.get("general").sections.get("general_section_6");
+    var section7 = Xrm.Page.ui.tabs.get("general").sections.get("general_section_7");
+    function sectionShow(show, hide, hide2) {
+        show.setVisible(true);
+        hide.setVisible(false);
+        hide2.setVisible(false);
+    }
+    function sectionHide() {
+        section5.setVisible(false);
+        section6.setVisible(false);
+        section7.setVisible(false);
+    }
+    switch (contactTypeValue) {
+        case 100000021 : //Student
+            sectionShow(section5, section6, section7);
+            break;
+        case 100000000 : //Teacher
+            sectionShow(section6, section5, section7);
+            break;
+        case 100000002 : //Coach
+            sectionShow(section7, section5, section6);
+            break;
+        case default :
+            sectionHide();
+            break;
+    }
+}

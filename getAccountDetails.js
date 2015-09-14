c4afc23982a49d028ecda002a3bebd2170ce00b2
Milton Reyes <miltonhreyes@gmail.com>
2015-09-14 14:27:22 -0400

    console.log

--- getAccountDetails.js
+++ getAccountDetails.js
@@ -1,17 +1,13 @@
 function getAccountDetails() {
-    var clientUrl = Xrm.Page.context.getClientUrl();
-    var lookUpObjectValue = Xrm.Page.getAttribute("parentcustomerid").getValue();
-    if ((lookUpObjectValue != null)) {
-        var lookUpTextValue = lookUpObjectValue[0].name;
-        var lookUpId = lookUpObjectValue[0].id;
+    var accountObject = Xrm.Page.getAttribute("parentcustomerid").getValue();
+    if ((accountObject != null)) {
+        var accountObjectId = accountObject[0].id;
         var clientUrl = Xrm.Page.context.getClientUrl();
         var ODATA_ENDPOINT = "/XRMServices/2011/OrganizationData.svc"; //Xrm OData end-point
         var odataSetName = "AccountSet";
         odataSetName = encodeURIComponent(odataSetName);
-        lookUpId = encodeURIComponent(lookUpId);
-        var odataSelect = clientUrl + ODATA_ENDPOINT + "/" + odataSetName + "(guid'" + lookUpId + "')";
-        //alert("Milton Reyes, your requested value is " + odataSetName);
-        //alert("Milton Reyes, your requested value is " + lookUpId);
+        accountObjectId = encodeURIComponent(accountObjectId);
+        var odataSelect = clientUrl + ODATA_ENDPOINT + "/" + odataSetName + "(guid'" + accountObjectId + "')";
         
         $.ajax({
             type: "GET",
@@ -21,14 +17,16 @@ function getAccountDetails() {
             beforeSend: function (XMLHttpRequest) { XMLHttpRequest.setRequestHeader("Accept", "application/json"); },
             success: function (data, textStatus, XmlHttpRequest) {
                 var resultContact = data.d;
+                console.log(data.d);
+                alert(Xrm.Page.context.getQueryStringParameters()[0]);
+                alert("Milton Reyes this: " + data.d.new_SubType.Value);
                 //alert(resultContact[0]);
                 //alert(ownerid);
                 //var mcCity1 = resultContact.Address1_City;
                 //replace the fields with the fields on your entity
                 //Xrm.Page.getAttribute("").setValue(mcCity1);
                 //Xrm.Page.getAttribute("").setValue(resultContact.new_subtype);
-                //Xrm.Page.getAttribute("").setValue(result_contact.Address1_Telephone1);
-                //alert(resultContact.new_subtype);
+                //Xrm.Page.getAttribute("").setValue(resultContact.Address1_Telephone1);
             },
             error: function (XmlHttpRequest, textStatus, errorThrown) { alert('OData Select Failed: ' + odataSelect); }
         });

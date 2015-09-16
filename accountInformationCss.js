5e65ff5ba041ae13973e1ef6ad65a33b4f5a2905
Milton Reyes <miltonhreyes@gmail.com>
2015-09-16 09:06:52 -0400

    documented accountInformationCss.js

--- accountInformationCss.js
+++ accountInformationCss.js
@@ -1,9 +1,11 @@
-function loadCss(path) {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
+//This function injects a CSS file into Dynamics CRM
+function loadCss(path) {  
+    var head = document.getElementsByTagName('head')[0]; //gets the first head tag by name
+    var link = document.createElement('link'); //creates a link element
+    link.rel = 'stylesheet'; //CSS stylesheet declaration in HTML
     link.type = 'text/css';
-    link.href = path;
+    link.href = path; //this calls the location of the CSS files
+    //usually if in Dynamics CRM, then it should be a WebResources file
     link.media = 'all';
     head.appendChild(link);
 }

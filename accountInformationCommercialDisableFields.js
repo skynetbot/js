c0142fafe7c8f52f435604db150d8d5a525ea80a
Milton Reyes <miltonhreyes@gmail.com>
2015-09-16 09:01:19 -0400

    documented accountInformationCommercialDisableFields.js

--- accountInformationCommercialDisableFields.js
+++ accountInformationCommercialDisableFields.js
@@ -1,30 +1,33 @@
-function schoolTypeStatus() {
+function schoolTypeStatus() { // Function to get school type value off of account entity
+    // if the school type is public the form should include a school code field
     var schoolTextValue = Xrm.Page.data.entity.attributes.get("new_subtype").getText();
     var schoolValue = Xrm.Page.data.entity.attributes.get("new_subtype").getValue();
     var schoolCodeField = Xrm.Page.ui.controls.get("new_schoolcode");
     if (schoolValue == 100000001) { //'Public'
-        $('#new_schoolcode_c').parent().show();
+        $('#new_schoolcode_c').parent().show(); // Shows the field <tr> which contains label and input
         schoolCodeField.setDisabled(false);
-        schoolCodeField.setRequiredLevel("required");
-    } else { //Private||Null
+        schoolCodeField.setRequiredLevel("required"); // Set this field as a must
+    } else { //Private||Null  as default value if nothing is selected hide the field
         $('#new_schoolcode_c').parent().hide();
         schoolCodeField.setRequiredLevel("none");
         schoolCodeField.setDisabled(true);
     }
 }
-function agreementStatus() {
+function agreementStatus() { // Function to display or hide the contract id.
+    // if there is an agreement there must be an contract id
     var agreementTextValue = Xrm.Page.data.entity.attributes.get("new_agreement").getText();
     var agreementValue = Xrm.Page.data.entity.attributes.get("new_agreement").getValue();
     var agreementNumberField = Xrm.Page.ui.controls.get("new_contractid");
     var agreementDueDateField = Xrm.Page.ui.controls.get("new_agreementexpirationdate");
-    if (agreementValue == 1) { //'Yes'
+    if (agreementValue == 1) { //'Yes'  if there is an agreement, display the due date and contract id
+        // for the agreement and set them as required fields
         $('#new_agreementexpirationdate_c').parent().show();
         $('#new_contractid_c').parent().show();
         agreementNumberField.setDisabled(false);
         agreementDueDateField.setDisabled(false);
         agreementNumberField.setRequiredLevel("required");
         agreementDueDateField.setRequiredLevel("required");
-    } else { //'No'
+    } else { //'No'  if there's no agreement hide the contract id and due date fields
         $('#new_agreementexpirationdate_c').parent().hide();
         $('#new_contractid_c').parent().hide();
         agreementDueDateField.setRequiredLevel("none");
@@ -33,7 +36,7 @@ function agreementStatus() {
         agreementNumberField.setDisabled(true);
     }
 }
-function affiliationStatusOld() {
+function affiliationStatusOld() { // This function is not used. See affiliationStatus()
     var affiliationTextValue = Xrm.Page.data.entity.attributes.get("new_affiliation").getText();
     var affiliationValue = Xrm.Page.data.entity.attributes.get("new_affiliation").getValue();
     var affiliationDioceseField = Xrm.Page.ui.controls.get("new_religiousordersubtype");
@@ -68,28 +71,31 @@ function affiliationStatusOld() {
         affiliationDioceseField.setDisabled(true);
     }
 }
-function affiliationStatus() {
+function affiliationStatus() { //Shows/Hides fields depending on affiliation status
     var affiliationTextValue = Xrm.Page.data.entity.attributes.get("new_affiliation").getText();
     var affiliationValue = Xrm.Page.data.entity.attributes.get("new_affiliation").getValue();
     var catholicField = Xrm.Page.ui.controls.get("new_catholicaffiliation");
     var protestantField = Xrm.Page.ui.controls.get("new_protestantaffiliation");
-    function hideProtestant() {
+    function hideProtestant() { //function to hide protestant affiliation fields
         protestantField.setRequiredLevel("none");
         protestantField.setDisabled(true);
         $('#new_protestantaffiliation_c').parent().hide();}
-    function hideCatholic() {
+    function hideCatholic() { //hides catholic affiliation fields
         catholicField.setRequiredLevel("none");
         catholicField.setDisabled(true);
         $('#new_catholicaffiliation_c').parent().hide();}
-    function showProtestant() {
+    function showProtestant() { 
+        //displays protestant affiliation fields and sets them as required
         $('#new_protestantaffiliation_c').parent().show();
         protestantField.setDisabled(false);
         protestantField.setRequiredLevel("required");}
     function showCatholic() {
+        // Displays catholic affiliation fields
         $('#new_catholicaffiliation_c').parent().show();
         catholicField.setDisabled(false);
         catholicField.setRequiredLevel("required");}
-    switch (affiliationValue) {
+    switch (affiliationValue) { // Depending on affiliation field
+            // makes funciton calls
         case 100000000: //'Catholic'
             hideProtestant();
             showCatholic();

53b28a8268986d6332008eeea1ecd818d1f87f6b
Milton Reyes <miltonhreyes@gmail.com>
2015-09-25 17:34:46 -0400

    update

--- contacts/contactFormScript.js
+++ contacts/contactFormScript.js
@@ -1,7 +1,3 @@
-var accountObject = getAttribute('parentcustomerid').getValue(),
-    clientUrl = Xrm.Page.context.getClientUrl(), //get CRM URL
-    ODATA_ENDPOINT = "/XRMServices/2011/OrganizationData.svc", //Xrm OData end-point
-    odataSetName = "AccountSet"; //This is found when exporting 
 function changeContactNumber() { // Previously Onload() now renamed to changeContactNumber. Copied to maintian backwards compatibility.
     // This function is probably useless since the field described here doesn't have any values
     // in the database.
@@ -82,42 +78,21 @@ function contactTypeOnChange(attribute, type) {
         }
     }
 }
-function alertObject() {
-    var obj = getAccountDetails();
-    console.log(obj.new_SubType.Value);
-}
-function changeThisName() {
-    objSchoolType = obj.new_SubType.Value;
-                // on account object account school type value hide or display related fields
-                switch (objSchoolType) {
-                    case 100000000: // Private if account is a private school
-                        enableField('new_contacttypeprivate');
-                        disableField('new_contacttypepublic');
-                        getAttribute('new_contacttypeprivate').addOnChange(contactTypeOnChange('new_contacttypeprivate', objSchoolType));
-                        break;
-                    case 100000001: // Public if account is a public school
-                        enableField('new_contacttypepublic');
-                        disableField('new_contacttypeprivate');
-                        // getAttribute('new_contacttypeprivate').addOnChange(oohlalah);
-                        // getAttribute('new_contacttypepublic').addOnChange(alert("on change"));
-                        break;
-                    default: // if account type is not defined hide both private and public fields
-                        disableField('new_contacttypepublic');
-                        disableField('new_contacttypeprivate');
-                        alert("Necesita identificar la cuenta " + accountObject[0].name + " como escuela publica o privada.");
-                        break;
-                }
-}
-/* Refer to Microsoft Dynamics CRM 2015 SDK:
-   {$SDK_Directory}\SampleCode\JS\RESTEndpoint\RESTJQueryContactEditor\
-   RESTJQueryContactEditor\Scripts\RESTJQueryContactEditor.js */
 function getAccountDetails() {
-    /* This function will get the related account value from the contact form
+    /* Refer to Microsoft Dynamics CRM 2015 SDK:
+       {$SDK_Directory}\SampleCode\JS\RESTEndpoint\RESTJQueryContactEditor\
+       RESTJQueryContactEditor\Scripts\RESTJQueryContactEditor.js
+       
+       This function will get the related account value from the contact form
        and with this value creates a database request to pull a specific value
        from the account entity (in this case if the account is from a 
        public/private/else) then with the requested value from the account
        displays certain fields and sets it's required permission in the current
        contact form */
+    var accountObject = getAttribute('parentcustomerid').getValue(),
+        clientUrl = Xrm.Page.context.getClientUrl(), //get CRM URL
+        ODATA_ENDPOINT = "/XRMServices/2011/OrganizationData.svc", //Xrm OData end-point
+        odataSetName = "AccountSet"; //This is found when exporting 
     if (!accountObject) {
         Xrm.Page.ui.setFormNotification('Developer: Error, could not retrieve the accountObject.', 'ERROR');
         return;
@@ -131,29 +106,87 @@ function getAccountDetails() {
     } else {
         odataSetName = encodeURIComponent(odataSetName);
     }
-    //account entity XML
-    odataSetName = encodeURIComponent(odataSetName); // prevent sql injection
-    accountObjectId = encodeURIComponent(accountObjectId); // prevent sql injection
+    // account entity XML
     var odataSelect = clientUrl + ODATA_ENDPOINT + "/" + odataSetName + "(guid'" + accountObjectId + "')";
     // odataSelect would be the select query statement
-    $.ajax({
-        type: "GET",
-        // HTTP GET request
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        url: odataSelect,
-        beforeSend: function (XMLHttpRequest) { XMLHttpRequest.setRequestHeader("Accept", "application/json"); },
-        success: successCallBack,
-        //function (data, textStatus, XmlHttpRequest) {
-            //var obj = JSON.parse(XmlHttpRequest.responseText).d;
-            //var obj = data.d;
-            //console.log(obj.new_SubType.Value);
-            // console.log(Xrm.Page.context.getQueryStringParameters());
-            // replace the fields with the fields on your entity
-            // Xrm.Page.getAttribute("").setValue(resultContact.new_subtype);
-        //},
-        error: function (XmlHttpRequest, textStatus, errorThrown) { alert('OData Select Failed: ' + odataSelect); }
-    });
-    console.log(objCall);
-    //console.log(objectAjax.responseJSON[0].d);
-}
+        $.ajax({
+            type: "GET",
+            // HTTP GET request
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: odataSelect,
+            beforeSend: function (XMLHttpRequest) { XMLHttpRequest.setRequestHeader("Accept", "application/json"); },
+            success: function (data, textStatus, XmlHttpRequest) {
+                // var obj = JSON.parse(XmlHttpRequest.responseText).d;
+                var obj = data.d;
+                console.log(obj);
+                if (!obj.new_SubType.Value) {
+                    Xrm.Page.ui.setFormNotification('La cuenta de este contacto no tiene determinado el tipo de instituci\u00F3n \
+como p\u00FAblico o privado.  Favor de editar este campo para mostrar informaci\u00F3n pertinente al contacto. Para editar haga \
+clic en ' + obj.Name + ' (abajo).', 'ERROR');
+                } else {
+                    var schoolType = obj.new_SubType.Value,
+                        studentSection = getSection('general', 'student_section'),
+                        teacherSection = getSection('general', 'teacher_section'),
+                        coachSection = getSection('general', 'coach_section');
+                    // on account object account school type value hide or display related fields
+                    if (schoolType == 100000000) { // Private if account is a private school
+                        enableField('new_contacttypeprivate');
+                        disableField('new_contacttypepublic');
+                        var typeValue = Xrm.Page.getAttribute('new_contacttypeprivate').getValue();
+                        Xrm.Page.getAttribute('new_contacttypeprivate').addOnChange(alert(typeValue));
+                        //    switch (Xrm.Page.getAttribute('new_contacttypeprivate').getValue()) {
+                        //        case 100000016: // Teacher
+                        //            //If a teacher display teacher section, hide not relevant
+                        //            sectionsHide(studentSection, coachSection);
+                        //            sectionShow(teacherSection);
+                        //            break;
+                        //        case 100000003: // Coach
+                        //            // if a coach, show coach section, hide not relevant
+                        //            sectionsHide(studentSection, teacherSection);
+                        //            sectionShow(coachSection);
+                        //            break;
+                        //        default:
+                        //            sectionsHide(studentSection, teacherSection, coachSection);
+                        //            break;
+                        //    });
+                    } else if (schoolType == 100000001) { // Public if account is a public school
+                        //enableField('new_contacttypepublic');
+                        //disableField('new_contacttypeprivate');
+                        //// contactTypeOnChange('new_contacttypepublic', schoolType);
+                        //// getAttribute('new_contacttypeprivate').addOnChange(oohlalah);
+                        //// getAttribute('new_contacttypepublic').addOnChange(alert("on change"));
+                        //var typeValue = Xrm.Page.getAttribute('new_contacttypepublic').getValue();
+                        //getAttribute('new_contacttypepublic').addOnChange(
+                        //    switch (Xrm.Page.getAttribute('new_contacttypepublic').getValue()) {
+                        //        case 100000000: // Teacher
+                        //            //If a teacher display teacher section, hide not relevant
+                        //            sectionsHide(studentSection, coachSection);
+                        //            sectionShow(teacherSection);
+                        //            break;
+                        //        case 100000021: //Student
+                        //            //If the contact type is a student, display the student section hide not relevant
+                        //            sectionsHide(teacherSection, coachSection);
+                        //            sectionShow(studentSection);
+                        //            break;
+                        //        case 100000002: // Coach
+                        //            // if a coach, show coach section, hide not relevant
+                        //            sectionsHide(studentSection, teacherSection);
+                        //            sectionShow(coachSection);
+                        //            break;
+                        //        default:
+                        //            sectionsHide(studentSection, teacherSection, coachSection);
+                        //            break;
+                        //    });
+                    } else {
+                        //disableField('new_contacttypepublic');
+                        //disableField('new_contacttypeprivate');
+                    }
+                } // SUCCESS BRACKET
+                // console.log(Xrm.Page.context.getQueryStringParameters());
+                // replace the fields with the fields on your entity
+                // Xrm.Page.getAttribute("").setValue(resultContact.new_subtype);
+            },
+            error: function (XmlHttpRequest, textStatus, errorThrown) { alert('OData Select Failed: ' + odataSelect); }
+        }); // END OF AJAX
+} // END OF getAccountDetails
